4f7787d49d63c68b55824687410c031dceef3b1f
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-09-07 21:28:17 +0330

    feat: add Button for keyboard buttons

---
+++ src/Messaging/Button.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace MahdiiMax\Telgeram\Messaging;
+
+class Button
+{
+    protected ?string $callbackData = null;
+    protected ?string $url = null;
+
+    public function __construct(
+        protected string $label
+    ) {}
+
+    public static function make(string $label): self
+    {
+        return new self($label);
+    }
+
+    public function callbackData(string $callbackData): self
+    {
+        $this->callbackData = $callbackData;
+        return $this;
+    }
+
+    public function url(string $url): self
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    /**
+     * @return array{text: string, callback_data?: string, url?: string}
+     */
+    public function toArray(): array
+    {
+        $payload = ['text' => $this->label];
+        if ($this->callbackData !== null) {
+            $payload['callback_data'] = $this->callbackData;
+        } elseif ($this->url !== null) {
+            $payload['url'] = $this->url;
+        }
+        return $payload;
+    }
+}
