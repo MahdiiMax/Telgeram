a01a3e081368f84c2fea6b638c2115daf9a846d4
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-09-15 22:04:08 +0330

    feat: add setWebhook and deleteWebhook API methods

--- src/Api/TelegramApi.php
+++ src/Api/TelegramApi.php
@@ -48,4 +48,21 @@ class TelegramApi
             'text' => $text,
         ], $extra));
     }
+
+    public function setWebhook(string $url, bool $dropPendingUpdates = false, ?string $secretToken = null): Response
+    {
+        $params = [
+            'url' => $url,
+            'drop_pending_updates' => $dropPendingUpdates,
+        ];
+        if ($secretToken !== null) {
+            $params['secret_token'] = $secretToken;
+        }
+        return $this->request('setWebhook', $params);
+    }
+
+    public function deleteWebhook(): Response
+    {
+        return $this->request('deleteWebhook');
+    }
 }
